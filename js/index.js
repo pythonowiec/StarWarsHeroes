0b57ba8722fb0915606dc7c88a1c3bd37e4debc0
pythonowiec <kuba.dobosz2004@gmail.com>
2020-09-19 18:16:29 +0200

    .

---
+++ js/index.js
@@ -0,0 +1,68 @@
+class StarWarsCharacters{
+    constructor() {
+        
+        this.characters = [];
+        this.API= "https://akabab.github.io/starwars-api/api/all.json";
+        this.API_PAGE = `${parseInt(Math.random() * 83 + 1)}`;
+
+        this.API_ENDPOINT = `${this.API}`;
+
+        this.catalog = null;
+
+        this.UiSelectors = {
+            content: '[data-content]'
+        }
+    } 
+
+    initializeCatalog() {
+        this.catalog = document.querySelector(this.UiSelectors.content);
+        
+        this.pullCharacters();
+            
+        
+        
+    }    
+
+    async pullCharacters() {
+        let arr = [];
+        let arrImage = [];
+        const characters  = await this.fetchData(this.API_ENDPOINT);
+        for (let index = 0; index < characters.length; index++) {
+            
+            const hero = document.createElement('div')
+            hero.id = 'hero';
+            this.catalog.appendChild(hero);
+
+            const nameHero = document.createElement('p');
+            nameHero.id = 'hero-name';
+            nameHero.innerHTML += characters[index].name;
+            hero.appendChild(nameHero);
+
+            const image = document.createElement('img');
+            image.id = 'hero-image';
+            image.setAttribute('src', characters[index].image);
+            hero.appendChild(image);
+
+            
+        }
+        
+        // this.characters = [...characters];
+
+        
+        console.log(characters);
+    }
+
+    async fetchData(url){
+        const response = await fetch(url);
+        const parseResponse = await response.json();
+        
+        return parseResponse;
+    }
+
+    createCatalog(characters){
+        this.catalog.innerHTML += `${characters}`;
+    }
+    createHero(element, index){
+        this.hero.innerHTML += element[index];
+    }
+} 
